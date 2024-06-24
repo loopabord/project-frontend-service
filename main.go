34cff563bb2f1c1cc8b6f66d874738799d078581
Aaron Goes <aaron.goes@hotmail.com>
2024-06-24 13:53:15 +0200

    Revert CORS changes

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"os"
 	projectv1 "projectfrontendservice/gen/project/v1"
 	"projectfrontendservice/gen/project/v1/projectv1connect"
-	"strings"
 
 	"connectrpc.com/connect"
 
@@ -160,18 +159,12 @@ func main() {
 	// Handle CORS headers
 	corsWrapper := func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Allow requests from any origin dynamically based on the request
-			allowedOrigins := "https://loopabord.nl, http://localhost:5173"
-			origin := r.Header.Get("Origin")
-			if strings.Contains(allowedOrigins, origin) {
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-			}
 			// Allow requests from any origin
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			// Allow specific headers
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Connect-Protocol-Version")
 			// Allow specific methods
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-			// Allow credentials
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			// Handle preflight requests
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
